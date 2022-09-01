cee8c416120b4dbcb14b7f1e58c6d1a643a7bae4
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-09-01 12:24:24 -0400

    importing the files to the app file as well as the react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import  {BrowserRouter, Route, Switch} from "react-router-dom"
 import Nav from "./nav/Nav";
-
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Services from "./components/Services/Services";
+import Contact from "./components/contact/Contact";
 
 import './App.css';
 
@@ -12,3 +16,4 @@ function App() {
 }
 
 export default App;
+
