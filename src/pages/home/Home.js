7275f5ad40e25294c3ce0ba6a4e7cf303c7b9e2a
Rigel <rigel.selmani@gmail.com>
2023-05-03 00:29:41 -0400

    info button & p

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,17 +4,21 @@ import logo from "../../images/reponsive-design.jpg"
 export default function Home(){
 
     return(
+      <div>
         <div className="home-content d-flex flex-row justify-content-center">
             <div className="home-section text-start" style={{width: "48%",margin:"90px 0 0 0"}}>
-              <div className="" style={{marginLeft:"34%"}}>
-                <h5 className="card-title mb-1">Mobile First</h5>
-                <p className="home-text mb-0">Responsive design & Development</p>
-                <p className="home-text">Make your Website & App work on desktop tablet mobile</p>
+              <div className="" style={{marginLeft:"42%"}}>
+                <h5 className="card-title mb-1 ">Mobile First</h5>
+                <p className="p-text mb-0">Responsive design & Development</p>
+                <p className="p-text">Make your Website & App work on desktop tablet mobile</p>
+                <button className="info-btn">More Info</button>
               </div>
             </div>
-            <div className="" style={{ width: "40%"}}>
+            <div className="responsive-section-img m-5" style={{ width: "40%"}}>
             <img src={logo} className="card-img-top" alt="home-img" style={{ width: "100%", height:"350px"}} />
             </div>
         </div>
+        <p className=" home-p-inovative p-4 fs-4 mx-auto justify-content-center" style={{width:"65%", }}>We deliver innovative solutios for enterprise to stay agile, competitive, operationally lean, and empowers them to do MORE.</p>
+      </div>
     )
 }
