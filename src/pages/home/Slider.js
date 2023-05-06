7752c6ecd9916b474b216a8f27f5b75799c91fe7
Rigel <rigel.selmani@gmail.com>
2023-05-06 19:23:13 -0400

    slider and style project in it

--- src/pages/home/Slider.js
+++ src/pages/home/Slider.js
@@ -1,6 +1,7 @@
-
-import CarpetDesk from "../../images/desktop-carpet.png"
-
+import "./Slider.css"
+import First from "../../images/first-app.png"
+import Second from "../../images/second-app.jpg"
+import Third from "../../images/third-app.png"
 export default function Slider (){
     return (
     <div id="carouselExampleCaptions" className="carousel slide p-3">
@@ -10,38 +11,34 @@ export default function Slider (){
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
       <div className="carousel-inner">
-        <div className="carousel-item active">
+            <div className="carousel-item active">
+              <div className=" bg-secondary" alt="..."  style={{height:"650px"}}>
+              <div className="d-flex flex-column carousel-caption d-none d-md-block p-3">
+                 <img src={First} className="pb-3" alt="..." style={{height:"500px",width:"70%", borderRadius:"10px"}}/>
+                 <h5 className="project">Pure Carpet</h5>
+                 <a className="btn-see-App text-white seeApp-home" href="https://carpet-64b3e.firebaseapp.com/">See App</a>
+              </div>
+              </div>
+            </div>
+        <div className="carousel-item">
           <div className=" bg-secondary" alt="..."  style={{height:"650px"}}>
             <div className="d-flex flex-column carousel-caption d-none d-md-block p-3">
-                 <img src={CarpetDesk} className="pb-3" alt="..." style={{height:"500px",width:"70%", borderRadius:"10px"}}/>
-                 <h5>First slide label</h5>
-                 <a className="text-white seeApp-home" href="https://carpet-64b3e.firebaseapp.com/">See App</a>
+                 <img src={Second} className="pb-3" alt="..." style={{height:"500px",width:"30%", borderRadius:"10px"}}/>
+                 <h5 className="project">Vanessa Nail SPA</h5>
+                 <a className="btn-see-App text-white seeApp-home" href="https://carpet-64b3e.firebaseapp.com/">See App</a>
             </div>
           </div>
         </div>
-        <div className="carousel-item">
-            <div className="carousel-item active">
+            <div className="carousel-item">
               <div className=" bg-secondary" alt="..."  style={{height:"650px"}}>
-                <div className="position-relative carousel-caption d-none d-md-block p-3">
-                <img src={CarpetDesk} className=" mt-3 pt-3 d-block w-10" alt="..." style={{height:"500px",width:"60%", borderRadius:"10px"}}/>
-                  <h5>Pure Carpet</h5>
-                  <p>Some representative placeholder content for the first slide.</p>
-                </div>
+              <div className="d-flex flex-column carousel-caption d-none d-md-block p-3">
+                 <img src={Third} className="pb-3" alt="..." style={{height:"500px",width:"70%", borderRadius:"10px"}}/>
+                 <h5 className="project">Tony HVAC</h5>
+                 <a className="btn-see-App text-white seeApp-home" href="https://carpet-64b3e.firebaseapp.com/">See App</a>
+            </div>
               </div>
             </div>
         </div>
-        <div className="carousel-item">
-            <div className="carousel-item active">
-              <div className=" bg-secondary" alt="..."  style={{height:"650px"}}>
-                <div className="position-relative carousel-caption d-none d-md-block p-3">
-                <img src={CarpetDesk} className=" mt-3 pt-3 d-block w-10" alt="..." style={{height:"500px",width:"60%", borderRadius:"10px"}}/>
-                  <h5>First slide label</h5>
-                  <p>Some representative placeholder content for the first slide.</p>
-                </div>
-              </div>
-            </div>
-        </div>
-      </div>
       <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Previous</span>
